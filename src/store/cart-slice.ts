04c3a284105b5863f36f962770e68e48281ac05a
kizuyoko <kizuyoko@gmail.com>
2024-03-22 11:08:53 +0100

    Logic to Reducer

--- src/store/cart-slice.ts
+++ src/store/cart-slice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 
 type CartItem = {
   id: string;
@@ -19,7 +19,32 @@ export const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    addToCart() {},
-    removeFromCart() {},
+    addToCart(state, action: PayloadAction<{ 
+      id: string;
+      title: string;
+      price: number;
+    }>) {
+      const itemIndex = state.items.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      if (itemIndex >= 0) {
+        state.items[itemIndex].quantity++;
+      } else {
+        state.items.push({...action.payload, quantity: 1});
+      }
+    },
+    removeFromCart(state, action: PayloadAction<string>) {
+      const itemIndex = state.items.findIndex(
+        (item) => item.id === action.payload
+      );
+
+      if (state.items[itemIndex].quantity === 1) {
+        state.items.splice(itemIndex, 1);
+      } else {
+        state.items[itemIndex].quantity--;
+      }
+
+    },
   }
 });
