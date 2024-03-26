657673265f12d47171b71c7d2fb11c9ff26cad2d
kizuyoko <kizuyoko@gmail.com>
2024-03-26 16:08:51 +0100

    Cart Items

--- src/components/CartItems.tsx
+++ src/components/CartItems.tsx
@@ -1,9 +1,26 @@
+import { type CartItem, addToCart, removeFromCart } from "../store/cart-slice";
+import { useCartDispatch, useCartSelector } from "../store/hooks";
+
 export default function CartItems() {
+  const cartItems = useCartSelector((state) => state.cart.items);
+  const dispatch = useCartDispatch();
+
+  const totalPrice = cartItems.reduce((val, item) => val + item.price * item.quantity, 0);
+  const formattedTotalPrice = totalPrice.toFixed(2);
+  
+  function handleAddToCart(item: CartItem) {
+    dispatch(addToCart(item));
+  }
+
+  function handleRemoveFromCart(id: string) {
+    dispatch(removeFromCart(id));
+  }
+
   return (
     <div id="cart">
-      <p>No items in cart!</p>
-
-      {/* <ul id="cart-items">
+      {cartItems.length === 0 && <p>No items in cart!</p>}
+      {cartItems.length > 0 &&  
+        <ul id="cart-items">
           {cartItems.map((item) => {
             const formattedPrice = `$${item.price.toFixed(2)}`;
 
@@ -23,11 +40,11 @@ export default function CartItems() {
               </li>
             );
           })}
-        </ul> */}
-
-      {/* <p id="cart-total-price">
-        Cart Total: <strong>{formattedTotalPrice}</strong>
-      </p> */}
+        </ul>
+      } 
+      <p id="cart-total-price">
+        Cart Total: <strong>${formattedTotalPrice}</strong>
+      </p>
     </div>
   );
 }
